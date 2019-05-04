2b6a44957c75bc3bc300ac1279314cefdb48addf
unknown <jonathanmohan.net@gmail.com>
2019-05-04 10:33:37 -0500

    interest elements

---
+++ ClinkedInSql/Models/Interest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinkedInSql.Models
+{
+    public class Interest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Interest(string name)
+        {
+            Name = name;
+        }
+
+        public Interest(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
