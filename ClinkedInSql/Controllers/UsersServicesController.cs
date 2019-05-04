c7d713e63dee38ef8a980b0430da0b9b59bda918
unknown <jonathanmohan.net@gmail.com>
2019-05-04 14:18:03 -0500

    User Services Elements

---
+++ ClinkedInSql/Controllers/UsersServicesController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ClinkedInSql.Data;
+using ClinkedInSql.Models;
+using ClinkedInSql.Validators;
+
+namespace ClinkedInSql.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserServicesController : ControllerBase
+    {
+        readonly UserServiceRepository _userServiceRepository;
+
+        public UserServicesController()
+        {
+            _userServiceRepository = new UserServiceRepository();
+        }
+
+
+        // Add User Service //
+        [HttpPost()]
+        public ActionResult AddUserService(CreateUserServiceRequest createRequest)
+        {
+
+            var newUserService = _userServiceRepository.AddUserService(createRequest.UserId, createRequest.ServiceId);
+
+            return Created($"api/userservices/{newUserService.Id}", newUserService);
+        }
+
+        // Get All User Services //
+        [HttpGet]
+        public ActionResult GetAllUserServices()
+        {
+            var userServices = _userServiceRepository.GetAll();
+
+            return Ok(userServices);
+        }
+
+
+        // Delete User Service //
+        [HttpDelete("{userId}")]
+        public ActionResult DeleteUserService(int userId)
+        {
+            _userServiceRepository.DeleteUserService(userId);
+
+            return Ok();
+        }
+
+
+        // Update User Service //
+        [HttpPut("{userId}")]
+        public ActionResult UpdateUserService(int userId, UpdateUserServiceRequest updateUserServiceRequest)
+        {
+            if (updateUserServiceRequest == null)
+            {
+                return BadRequest(new { error = "We Need More Info About Your Service" });
+            }
+            var updatedUser = _userServiceRepository.UpdateUserService(
+                userId,
+                updateUserServiceRequest.UserId,
+                updateUserServiceRequest.ServiceId);
+
+            return Ok();
+        }
+    }
+}
---
+++ ClinkedInSql/Data/UserServiceRepository.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClinkedInSql.Models;
+using ClinkedInSql.Data;
+using System.Data.SqlClient;
+
+namespace ClinkedInSql.Data
+{
+    public class UserServiceRepository
+    {
+        const string ConnectionString = "Server = localhost; Database = ClinkedIn; Trusted_Connection = True;";
+
+        // Add User Service //
+        public UserService AddUserService(int userId, int serviceId)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                var insertUserServiceCommand = connection.CreateCommand();
+                insertUserServiceCommand.CommandText = $@"Insert into userservices (userId, serviceId)
+                                              Output inserted.*
+                                              Values(@userId, @serviceId)";
+
+                insertUserServiceCommand.Parameters.AddWithValue("userId", userId);
+                insertUserServiceCommand.Parameters.AddWithValue("serviceId", serviceId);
+
+                var reader = insertUserServiceCommand.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    var insertedUserId = (int)reader["userId"];
+                    var insertedServiceId = (int)reader["serviceId"];
+
+                    var insertedId = (int)reader["Id"];
+
+                    var newUserService = new UserService(insertedUserId, insertedServiceId) { Id = insertedId };
+
+                    return newUserService;
+                }
+            }
+
+            throw new Exception("No userservice found");
+        }
+
+
+        // Get All User Services //
+        public List<UserService> GetAll()
+        {
+            var userServices = new List<UserService>();
+
+            var connection = new SqlConnection(ConnectionString);
+            connection.Open();
+
+            var getAllUserServicesCommand = connection.CreateCommand();
+            getAllUserServicesCommand.CommandText = @"select userId, serviceId, id
+                                                    from userservices";
+
+            var reader = getAllUserServicesCommand.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var id = (int)reader["Id"];
+                var userId = (int)reader["userId"];
+                var serviceId = (int)reader["serviceId"];
+
+                var userservice = new UserService(userId, serviceId) { Id = id };
+
+                userServices.Add(userservice);
+            }
+
+            connection.Close();
+
+            return userServices;
+        }
+
+
+        // Delete User Service //
+        public void DeleteUserService(int userServiceId)
+        {
+            var connection = new SqlConnection(ConnectionString);
+            connection.Open();
+
+            var deleteUserServiceCommand = connection.CreateCommand();
+            deleteUserServiceCommand.Parameters.AddWithValue("Id", userServiceId);
+            deleteUserServiceCommand.CommandText = @"Delete
+                                                    From UserServices
+                                                    Where Id = @Id";
+
+            deleteUserServiceCommand.ExecuteNonQuery();
+
+            connection.Close();
+        }
+
+
+        // Update User Services //
+        public bool UpdateUserService(int id, int userId, int serviceId)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                var updateUserServiceCommand = connection.CreateCommand();
+
+                updateUserServiceCommand.Parameters.AddWithValue("@Id", id);
+
+                updateUserServiceCommand.CommandText = $@"Update userservices
+                                                            Set userId = @userId,
+                                                            serviceId = @serviceId
+                                                            Where Id = @Id";
+
+                updateUserServiceCommand.Parameters.AddWithValue("userId", userId);
+                updateUserServiceCommand.Parameters.AddWithValue("serviceId", serviceId);
+
+
+                var numberOfRowsUpdated = updateUserServiceCommand.ExecuteNonQuery();
+
+                connection.Close();
+
+                if (numberOfRowsUpdated > 0)
+                { return true; }
+                return false;
+            }
+        }
+    }
+}
